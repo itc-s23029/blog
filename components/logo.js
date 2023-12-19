715ab92ef6b5e607ef6a8c3938af49b1e8ec933c
itc-s23029 <s23029@std.it-college.ac.jp>
2023-12-19 07:32:45 +0000

    3.6owari
    ９４ページまでおわた

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import styles from 'styles/logo.module.css'
 
-const Logo = () => {
+const Logo = ({ boxOn = false }) => {
   return (
     <Link href='/'>
-      CUBE
+      <href className={boxOn ? styles.box : styles.basic}>CUBE</href>
     </Link>
   )
 }
