c6fd86615f9bd6f9bf41195bd894f9160b187c6e
itc-s23029 <s23029@std.it-college.ac.jp>
2023-12-20 07:24:33 +0000

    4syuowata
    ４章おわた

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,15 @@
+import Container from 'components/container'
 import Logo from 'components/logo'
+import styles from 'styles/footer.module.css'
 const Footer = () => {
   return (
-    <footer>
-      <Logo />
+    <footer className={styles.wrapper}>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo />
+          [ソーシャル]
+        </div>
+      </Container>
     </footer>
   )
 }
