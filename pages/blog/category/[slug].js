3dceeab9fbdf22c76b5ebe839b5d7f1cff1b375f
itc-s23029 <s23029@std.it-college.ac.jp>
2024-01-21 06:01:56 +0000

    9.11
    ９章おわた

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,15 +1,21 @@
-import { getAllCategories } from 'lib/api'
+import { getAllCategories, getAllPostsByCategory } from 'lib/api'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Category = ({ name }) => {
+import { eyecatchLocal } from 'lib/constants'
+
+const Category = ({ name, posts }) => {
   return (
     <Container>
+      <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
       <PostHeader title={name} subtitle='Blog Category' />
+      <Posts posts={posts} />
     </Container>
   )
 }
-
 export default Category
 
 const getStaticPaths = async () => {
@@ -28,11 +34,20 @@ const getStaticProps = async context => {
   const allCats = await getAllCategories()
   const cat = allCats.find(({ slug }) => slug === catSlug)
 
+  const posts = await getAllPostsByCategory(cat.id)
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
-      name: cat.name
+      name: cat.name,
+      posts
     }
   }
 }
-
 export { getStaticProps }
